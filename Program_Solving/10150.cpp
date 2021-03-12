3fe460f86bfb0a81b88e5fd7dfbe2ed334dbd5e9
ddings73 <ddings7303@gmail.com>
2021-03-12 23:50:59 +0900

    Doublets 문제 풀이 추가.

--- Program_Solving/10150.cpp
+++ Program_Solving/10150.cpp
@@ -2,120 +2,100 @@
 #include<string>
 #include<vector>
 #include<queue>
-#include<algorithm>
 using namespace std;
 #pragma warning(disable:4996);
 
-int Distance[25143];
+int idx;
+int Parent[25143];
+string StartWord, EndWord;
 string WordDictionary[25143];
 vector<string> OUTPUT;
 
-bool isDoublet(string Comparator, string Target) {
-	int length_diff = Comparator.length() - Target.length();
-		
-	length_diff = abs(length_diff);
-
-	if (length_diff > 0) return false;
+bool HasInclude(string now, int size) {
+	for (int i = 0; i < size;i++) {
+		if (now == WordDictionary[i]) return true;
+	}
+	return false;
+}
+int isDoublet(string Comparator, string Target) {
+	int counter = 0;
 
 	for (int i = 0; i < Comparator.size(); i++) {
-		if (i > Target.length()) break;
-		if (Comparator[i] != Target[i]) length_diff++;
+		if (Comparator[i] != Target[i]) counter++;
 	}
 
-	if (length_diff > 1) return false;
-
-	return true;
+	return counter;
 }
 
-void DistanceSetting(int DictionarySize) {
+
+void solve(int DictionarySize) {
+	if (StartWord.length() != EndWord.length()) return;
+	if (HasInclude(StartWord, DictionarySize) == false || HasInclude(EndWord, DictionarySize) == false) return;
+
+	queue<int>Sub_Dictionary;
+
+	int StartWordIdx;
+
+
+	// 길이가 동일한 단어 솎아내기
 	for (int i = 0; i < DictionarySize; i++) {
-		Distance[i] = 26000;
-	}
-}
+		string item = WordDictionary[i];
 
-bool cmp(string Comparator, string Target) {
-	if (Comparator.length() < Target.length())
-		return true;
-	return false;
-}
+		if (item == StartWord) {
+			StartWordIdx = i;
+		}
 
-bool DoubletCmp(int Comparator, int Target){
-	if (Distance[Comparator] < Distance[Target])
-		return true;
-	return false;
-}
 
-void solve(string StartWord, string EndWord, int DictionarySize) {
-	vector<int> Doublet[25143];
-
-	int Start_pos = -1, End_pos = -1;
-
-	sort(WordDictionary, WordDictionary + DictionarySize - 1, cmp);
-
-	for (int i = 0; i < DictionarySize; i++) {
-		if (WordDictionary[i] == StartWord) 
-			Start_pos = i;
-
-		if (WordDictionary[i] == EndWord)
-			End_pos = i;
-
-		for (int j = i + 1; j < DictionarySize; j++) {
-			if (WordDictionary[i].length() != WordDictionary[j].length()) 
-				break;
-
-			if (isDoublet(WordDictionary[i], WordDictionary[j])) {
-				Doublet[i].push_back(j);
-				Doublet[j].push_back(i);
-			}
+		if (item.length() == StartWord.length()) {
+			Sub_Dictionary.push(i);
 		}
 	}
-
-
-	if (Start_pos == -1 || End_pos == -1) 
-		return;
-
-	queue<int> q;
-	q.push(End_pos);
 	
-	DistanceSetting(DictionarySize);
+	Parent[StartWordIdx] = -1;
+	queue<int> words;
 
-	Distance[End_pos] = 0;
+	words.push(StartWordIdx);
+	while (!words.empty()) {
+		int now = words.front();
+		words.pop();
 
-	while (!q.empty()) {
-		int now = q.front();
-		q.pop();
+		string value = WordDictionary[now];
 
-		if (now == Start_pos) continue;
+		if (isDoublet(value, EndWord) < 2) {
+			OUTPUT.push_back(EndWord);
 
-		for (int nxt : Doublet[now]) {
-			if (Distance[nxt] > Distance[now] + 1) {
-				Distance[nxt] = Distance[now] + 1;
-				q.push(nxt);
+			while (Parent[now] != -1) {
+				string value = WordDictionary[now];
+				OUTPUT.push_back(value);
+				now = Parent[now];
 			}
+
+			OUTPUT.push_back(StartWord);
+			break;
 		}
+
+		int size = Sub_Dictionary.size();
+
+		for (int i = 0; i < size; i++) {
+			int nxt = Sub_Dictionary.front();
+			Sub_Dictionary.pop();
+
+			string nxt_value = WordDictionary[nxt];
+			if (value == nxt_value) continue;
+
+			if (isDoublet(value, nxt_value) < 2) {
+				Parent[nxt] = now;
+				words.push(nxt);
+				continue;
+			}
+
+			Sub_Dictionary.push(nxt);
+		}
+
 	}
-
-	for (int i = 0; i < DictionarySize; i++) {
-		sort(Doublet[i].begin(), Doublet[i].end(), DoubletCmp);
-	}
-
-	q.push(Start_pos);
-	OUTPUT.push_back(StartWord);
-
-	while (!q.empty()) {
-		int now = q.front();
-		q.pop();
-
-		if (now == End_pos || Doublet[now].size() == 0) break;
-
-		int nxt = Doublet[now][0];
-		if (Distance[nxt] != Distance[Doublet[nxt][0]] + 1) break;
-
-		OUTPUT.push_back(WordDictionary[nxt]);
-		q.push(nxt);
-	}
-	OUTPUT.push_back(EndWord);
 }
+	
+
 
 int main() {
 	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
@@ -125,33 +105,43 @@ int main() {
 	string str;
 
 	int end = 0;
+	int count = 0;
 	while (getline(cin, str)) {
 		
 		int space = str.find(" ");
 
 		if (space < str.length()) {
+			if (count > 0) {
+				cout << '\n';
+			}
 
-			string str2 = str.substr(space + 1, str.length());
-			str = str.substr(0, space);
+			EndWord = str.substr(space + 1, str.length());
+			StartWord = str.substr(0, space);
 			
 			OUTPUT.clear();
 
-			solve(str, str2, end);
+			solve(end);
 
-			if(OUTPUT.size() <= 2){
+			if(OUTPUT.size() < 2){
 				cout << "No solution.\n";
 			}
 			else {
-				for (string str : OUTPUT)
-					cout << str << '\n';
+				while (!OUTPUT.empty()) {
+					cout << OUTPUT.back() << '\n';
+
+					OUTPUT.pop_back();
+				}
+				
 			}
-			cout << '\n';
+
+			count++;
 		}
 		else {
 			WordDictionary[end] = str;
 			end++;
 		}
 	}
+
 
 	return 0;
 }
